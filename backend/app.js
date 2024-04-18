af667407100a78bf03b368f52d5472a70d2ecc7f
poornachandran872 <chandru08july2000@gmail.com>
2024-04-18 18:12:59 +0530

    backend auth added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser')
+const errorMiddleWare = require('./middlewares/error');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieParser());
+
 const check = (req,res) =>{
     res.json({
         success:true,
@@ -10,4 +14,5 @@ const check = (req,res) =>{
     })
 }
 app.use('/api/v1',check);
+app.use(errorMiddleWare)
 module.exports = app;
